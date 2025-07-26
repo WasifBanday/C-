ca488235ee09c390f6d7669ea65e463aae80c69c
wasif <wasifahmad679@gmail.com>
2025-07-26 17:16:38 +0530

    done basics

--- Basics.cpp
+++ Basics.cpp
@@ -3,12 +3,18 @@ using namespace std;
 int main() {
 
     // std::cout<<"Hello world";
-    cout<<"Hello world";
-    cout<<"Hello world";
-    cout<<"Hello world";
-    cout<<"Hello world";
-    cout<<"Hello world";
-    cout<<"Hello world";
+    // cout<<"Hello world";
+
+    //  variables
+    int value; // decleration
+    value = 100; // initilation
+
+    int x = 100;
+
+    cout<<value<<endl;
+    cout<<x<<endl;
+
+    cout<<value<<endl<<x;
 
 
 
